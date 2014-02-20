b2d8ead8cf3269373de79c8ae52e91391be8c2f7
Jesus Jimenez <jesjimenez@gmail.com>
2014-02-20 13:58:18 +0100

    Reestructurado código, limpieza general y retabulación

--- codigo.js
+++ codigo.js
@@ -23,6 +23,7 @@ function pad2(n) {
 
 // Devuelve una fecha en formato YYYYMMDD
 function soloFecha(d) {
+// moment es leeeeeeeento
 //  return moment(d).format("YYYYMMDD");
   return ''+d.getFullYear()+pad2(d.getMonth()+1,2)+pad2(d.getDate(),2);
 }
@@ -37,33 +38,33 @@ function timeFormat(formats) {
 
 // Ajusta los ticks del eje según el nivel de ampliación
 function ajustar(axis,extent) {
-  intervalo=extent[1]-extent[0];
-  // > 3 días, sólo mostramos los días
-  f={tipo:d3.time.days,interv:1};
-  //axis=axis.ticks(d3.time.hours,4);
-  // Entre 2 días y 5 días
-  if (intervalo <5*86400000) {
-	 f.tipo=d3.time.hours;f.interv=4;
-  }
-  // Entre 12h y 2 días
-  if (intervalo <2*86400000) {
-		 f.tipo=d3.time.hours;f.interv=1;
-	  }
-  // Entre 6h y 12h
-  if (intervalo <86400000/2) {
-	 f.tipo=d3.time.minutes;f.interv=30;
-  }
-  // Entre 3h y 6h
-  if (intervalo <86400000/4) {
-	 f.tipo=d3.time.minutes;f.interv=15;
-  }
-  // Menos de 3h
-  if (intervalo <86400000/8) {
-	 f.tipo=d3.time.minutes;f.interv=5;
-  }
-  
-  axis=axis.ticks(f.tipo,f.interv);
-  return axis;
+	intervalo=extent[1]-extent[0];
+	// > 3 días, sólo mostramos los días
+	f={tipo:d3.time.days,interv:1};
+	//axis=axis.ticks(d3.time.hours,4);
+	// Entre 2 días y 5 días
+	if (intervalo <5*86400000) {
+		f.tipo=d3.time.hours;f.interv=4;
+	}
+	// Entre 12h y 2 días
+	if (intervalo <2*86400000) {
+		f.tipo=d3.time.hours;f.interv=1;
+	}
+	// Entre 6h y 12h
+	if (intervalo <86400000/2) {
+		f.tipo=d3.time.minutes;f.interv=30;
+	}
+	// Entre 3h y 6h
+	if (intervalo <86400000/4) {
+		f.tipo=d3.time.minutes;f.interv=15;
+	}
+	// Menos de 3h
+	if (intervalo <86400000/8) {
+		f.tipo=d3.time.minutes;f.interv=5;
+	}
+
+	axis=axis.ticks(f.tipo,f.interv);
+	return axis;
 }
 
 // Cambiar tamaño de los gráficos cuando se redimensione la ventana
@@ -128,17 +129,18 @@ var x = d3.time.scale().range([0, width]),
     y2 = d3.scale.linear().range([height2, 0]);
 
 var customTimeFormat = timeFormat([
-  [d3.time.format("%Y"), function() { return true; }],
-//  [d3.time.format("%b"), function(d) { return d.getMonth(); }],
-  [d3.time.format("%b %d"), function(d) { return d.getDate() == 1; }],
-//  [d3.time.format("%a %d"), function(d) { return d.getDay() && d.getDate() != 1; }],
-  [d3.time.format(""), function(d) { return (d.getHours()==0); }],  // No mostrar las 0h
-  [d3.time.format("%H"), function(d) { return d.getHours(); }],
-  [d3.time.format("%H:%M"), function(d) { return d.getMinutes(); }],
-/*  [d3.time.format(":%S"), function(d) { return d.getSeconds(); }],
-  [d3.time.format(".%L"), function(d) { return d.getMilliseconds(); }]*/
-]);    
-	 // Eje x superior
+		[d3.time.format("%Y"), function() { return true; }],
+		//  [d3.time.format("%b"), function(d) { return d.getMonth(); }],
+		[d3.time.format("%b %d"), function(d) { return d.getDate() == 1; }],
+		//  [d3.time.format("%a %d"), function(d) { return d.getDay() && d.getDate() != 1; }],
+		[d3.time.format(""), function(d) { return (d.getHours()==0); }],  // No mostrar las 0h
+		[d3.time.format("%H"), function(d) { return d.getHours(); }],
+		[d3.time.format("%H:%M"), function(d) { return d.getMinutes(); }],
+		/*  [d3.time.format(":%S"), function(d) { return d.getSeconds(); }],
+			 [d3.time.format(".%L"), function(d) { return d.getMilliseconds(); }]*/
+		]);    
+	 
+// Eje x superior
 //	var xAxis = d3.svg.axis().scale(x).orient("bottom").tickFormat(customTimeFormat),
   //TODO:Formato variable que muestra minutos o no en función del tamaño/zoom
   //TODO:Formato variable para xAxis2 que oculte días cuando hay muchos meses en pantalla
@@ -156,22 +158,22 @@ var frmDetalle=timeFormat([
 ]);
 
 var xAxis = d3.svg.axis()
-	 .scale(x)
-	 .orient("bottom")
-	 .tickFormat(frmDetalle)
-	 .ticks(d3.time.hours,1),
-    // Eje x inferior
-    xAxis2 = d3.svg.axis().scale(x2).orient("bottom").tickFormat(frmDias).ticks(d3.time.days,1),    
-    // Eje y inferior
-    yAxis2 = d3.svg.axis().scale(y2).orient("left"),
-    // Eje y superior
-    yAxis = d3.svg.axis().scale(y).orient("left"),
-    // Eje de meses
-    xAxisDias=d3.svg.axis().scale(x2).orient("bottom")
-    	.ticks(d3.time.months,1)
-    	.tickFormat(d3.time.format("%b"))
-    	.tickSize(-height2-40);
-//	xAxisDias.selectAll("line").attr("transform", "translate(0,-50)");
+    .scale(x)
+    .orient("bottom")
+    .tickFormat(frmDetalle)
+    .ticks(d3.time.hours,1),
+  // Eje x inferior
+  xAxis2 = d3.svg.axis().scale(x2).orient("bottom").tickFormat(frmDias).ticks(d3.time.days,1),    
+  // Eje y inferior
+  yAxis2 = d3.svg.axis().scale(y2).orient("left"),
+  // Eje y superior
+  yAxis = d3.svg.axis().scale(y).orient("left"),
+  // Eje de meses
+  xAxisDias=d3.svg.axis().scale(x2).orient("bottom")
+    .ticks(d3.time.months,1)
+    .tickFormat(d3.time.format("%b"))
+    .tickSize(-height2-40);
+  //	xAxisDias.selectAll("line").attr("transform", "translate(0,-50)");
 	
     
 // Brush para calcular consumo de una franja
@@ -181,52 +183,50 @@ var brushdet=d3.svg.brush()
 		
 // Brush para la gráfica inferior
 var brush = d3.svg.brush()
-    .x(x2)
-    .on("brush", brushed)
-	 .on("brushend", brushend);
-	 /*var brush2= d3.svg.brush()
-	 .x(x2)
-	 .on("brushend",brushed2);*/
+  .x(x2)
+  .on("brush", brushed)
+	.on("brushend", brushend);
+	/*var brush2= d3.svg.brush()
+	.x(x2)
+	.on("brushend",brushed2);*/
 
 // Área gráfica superior
 var area = d3.svg.area()
-    .interpolate("monotone")
-    .x(function(d) { return x(d.date); })
-    .y0(height)
-    .y1(function(d) { return y(d.w); });
-
-//Área gráfica inferior
-/*var area2 = d3.svg.area()
-    .interpolate("monotone")
-    .x(function(d) { return x2(d.date); })
-    .y0(height2)
-    .y1(function(d) { return y2(d.w); });*/
+  .interpolate("monotone")
+  .x(function(d) { return x(d.date); })
+  .y0(height)
+  .y1(function(d) { return y(d.w); });
 
 var svgsup = d3.select("#graficasup").append("svg")
-    .attr("width", width + margin.left + margin.right)
-    .attr("height", height + margin.top + margin.bottom);
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom);
 
 var svginf = d3.select("#graficainf").append("svg")
-	 .attr("width", width + margin.left + margin.right)
-	 .attr("height", height2 + margin.top + margin.bottom);
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height2 + margin.top + margin.bottom);
 	 
 svgsup.append("defs").append("clipPath")
-    .attr("id", "clip")
+  .attr("id", "clip")
   .append("rect")
-    .attr("width", width)
-    .attr("height", height);
+  .attr("width", width)
+  .attr("height", height);
 
 // Grafica superior
 var focus = svgsup.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");	 
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");	 
 	 
 // Grafica inferior
 var context = svginf.append("g")
-    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
-
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+	
+// Cargar CSV
 d3.csv("consumo.csv")
-	.row(function(d) {return {date:parseDate(d.date),w:+d.w}})
-	.get(function(error, data) {
+  .row(function(d) {return {date:parseDate(d.date),w:+d.w}})
+  .get(iniciarGraficas);
+
+// Función de callback que se ejecuta cuando los datos estén cargados
+// Inicia y dibuja todas las gráficas
+function iniciarGraficas(error, data) {
 
   datos=data;
   
@@ -236,31 +236,28 @@ d3.csv("consumo.csv")
   // Crear un array para acelerar las búsquedas por fecha
   lookup={};
   for (var i = 0; i < datos.length; i++) {
-	 amd=soloFecha(datos[i].date);
-	 if (!(amd in lookup))
-		lookup[amd] = i;
+    amd=soloFecha(datos[i].date);
+    if (!(amd in lookup))
+      lookup[amd] = i;
   }
   
   // Calcular kWh por día
   datos.forEach(function(d) {
-	 ind=soloFecha(d.date);
-	 if (ind in consdiario)
-		consdiario[ind]+=d.w/60000;
-	 else
-		consdiario[ind]=d.w/60000;
+    ind=soloFecha(d.date);
+    if (ind in consdiario)
+      consdiario[ind]+=d.w/60000;
+    else
+      consdiario[ind]=d.w/60000;
   });
+
   // Convertir a un array apto para D3
   cd=[];
   var pd = d3.time.format("%Y%m%d").parse;  
   for (d in consdiario) {
-	 cd.push({date:pd(d),kwh:consdiario[d]});
+    cd.push({date:pd(d),kwh:consdiario[d]});
   }
   // Ordenar por fecha por si las moscas
   cd.sort(function(a,b){return a.date-b.date;});
-  
-  //alert(maxp+" "+maxd);
-
-  
   
 /*  aux=datos.map(function(d){return {x:d.date.getTime(),y:d.w}});
   simpli=simplify(aux,3).map(function(d){return {date:new Date(d.x),w:d.y}});*/
@@ -288,59 +285,36 @@ d3.csv("consumo.csv")
   // focus es el gráfico grande superior
   
   focus.append("path")
-      .datum(detalle)
-      .attr("clip-path", "url(#clip)")
-      .attr("d", area)
-		.attr("id","grafica");
-
-/*  anchobarra=x(cd[1].date);
-  focus.selectAll(".bar")
-	 .data(cd)
-	 .enter()
-		.append("rect")
-		.attr("class","bar")
-		.attr("x",function(d) {return x(d.date);})
-		.attr("width",anchobarra)
-		.attr("y",function(d) {return y(d.w);})
-		.attr("height", function(d) { return height - y(d.w); });*/
-		
+    .datum(detalle)
+    .attr("clip-path", "url(#clip)")
+    .attr("d", area)
+    .attr("id","grafica");
 
   // Eje x
   focus.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height + ")")
-      .call(xAxis);
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
 
   focus.append("g")
-      .attr("class", "y axis")
-      .call(yAxis);
+    .attr("class", "y axis")
+    .call(yAxis);
 	
-	dibujarMax(mostrarmax);
-	dibujarZonasHorarias(mostrarZonas=="TDH");		
+  dibujarMax(mostrarmax);
+  dibujarZonasHorarias(mostrarZonas=="TDH");		
 	 
-	
-		
-/*  focus.append("g")
-		.attr("class","x brush")
-		.call(brush2)
-		.selectAll("rect")
-		  .attr("y",-6)
-		  .attr("height",height+7);*/
-		
-  
-//  focus.selectAll("line").data(y2.ticks(10)).enter().append("line").attr("x1",0).attr("x2",100).attr("y1",y2).attr("y2",y2).style("stroke","#ccc");
   // context es el gráfico inferior
 
   anchobarra=x2(cd[1].date);
   aux=context.selectAll(".bar")
-  .data(cd)
-  .enter();
+    .data(cd)
+    .enter();
   aux.append("rect")
-	 .attr("class","bar")
-	 .attr("x",function(d) {return x2(d.date)+1;})
-	 .attr("width",anchobarra-2)
-	 .attr("y",function(d) {return y2(d.kwh);})
-	 .attr("height", function(d) { return height2 - y2(d.kwh); });
+    .attr("class","bar")
+    .attr("x",function(d) {return x2(d.date)+1;})
+    .attr("width",anchobarra-2)
+    .attr("y",function(d) {return y2(d.kwh);})
+    .attr("height", function(d) { return height2 - y2(d.kwh); });
   
   // Mostrar etiquetas en las barras con el importe en eur
   // De momento no lo pongo porque queda raro
@@ -349,54 +323,52 @@ d3.csv("consumo.csv")
 		.attr("y",function(d) {return y2(d.kwh)-1;})
 		.attr("class","etibarra")
 		.text(function(d) {return (d.kwh*eurporkwh).toFixed(1)+"€"});*/
-  
 
   context.append("g")
-      .attr("class", "x axis")
-      .attr("transform", "translate(0," + height2 + ")")
-      .call(xAxis2);
+    .attr("class", "x axis")
+    .attr("transform", "translate(0," + height2 + ")")
+    .call(xAxis2);
   // eje inferior de días
   context.append("g")
-		.attr("class", "x axis2")
-		.attr("transform", "translate(0," + (height2+30) + ")").call(xAxisDias);
+    .attr("class", "x axis2")
+    .attr("transform", "translate(0," + (height2+30) + ")").call(xAxisDias);
   context.append("g")
-		.attr("class","y axis")
-		.call(yAxis2);
+    .attr("class","y axis")
+    .call(yAxis2);
   // Rótulo del eje y
   context.append("text")
-	 .attr("class","rotuloy")
-/*	 .style("writing-mode","tb")
-	 .style("glyph-orientation-vertical","0")*/
-	 .text("kWh")
-	 .attr("x",5);
+    .attr("class","rotuloy")
+  /*	 .style("writing-mode","tb")
+    .style("glyph-orientation-vertical","0")*/
+    .text("kWh")
+    .attr("x",5);
 	 
   var brushg=context.append("g")
-      .attr("class", "x brush")
-      .call(brush);
+    .attr("class", "x brush")
+    .call(brush);
   brushg.selectAll("rect")
-      .attr("y", -6)
-      .attr("height", height2 + 7);
+    .attr("y", -6)
+    .attr("height", height2 + 7);
 
   var brushdetg=focus.append("g")
-		.attr("class","x brush")
-		.call(brushdet);
-	brushdetg.selectAll("rect")
-		.attr("y", 0)
-		.attr("height", height+1);
+    .attr("class","x brush")
+    .call(brushdet);
+  brushdetg.selectAll("rect")
+    .attr("y", 0)
+    .attr("height", height+1);
   brushdetg.insert("text",".background")
-		.text("aaaa")
-		.attr("id","kwhsel")
-		.attr("style","display:none");
-		
+    .text("aaaa")
+    .attr("id","kwhsel")
+    .attr("style","display:none");		
 
   // Cambiamos los handles
 /*  var arc = d3.svg.arc()
-		.outerRadius(height2 / 4)
-		.startAngle(0)
-		.endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
+      .outerRadius(height2 / 4)
+      .startAngle(0)
+      .endAngle(function(d, i) { return i ? -Math.PI : Math.PI; });
   brushg.selectAll(".resize").append("path")
-		.attr("transform", "translate(0," +  (6+(height2 / 4)) + ")")
-		.attr("d", arc);*/
+    .attr("transform", "translate(0," +  (6+(height2 / 4)) + ")")
+    .attr("d", arc);*/
 
 /*		context.selectAll(".resize rect")
 		  .style("visibility","visible");*/
@@ -406,8 +378,8 @@ d3.csv("consumo.csv")
   // Activar eventos y configuraciones iniciales
   checkmax=d3.select("#mostrarmax");
   checkmax.on("change",function() {
-	 mostrarmax=d3.select("#mostrarmax").property("checked");	 
-	 dibujarMax(mostrarmax);
+    mostrarmax=d3.select("#mostrarmax").property("checked");	 
+    dibujarMax(mostrarmax);
   });
   checkmax.property("checked",mostrarmax);
   
@@ -427,16 +399,16 @@ d3.csv("consumo.csv")
   d3.select("#graficainf .brush").call(brush);
   
   recalcularEstadisticas();  
-});
+}
 
 // Aplica las transformaciones manuales que no se pueden hacer con CSS, 
 // y que se perderían ante cualquier actualización del gráfico
 function transformaciones() {
   // Justificación izquierda de labels de xAxisMeses
   context.selectAll(".x.axis2 text")
-		.attr("x",6)
-		.attr("y",0)
-		.style("text-anchor","start");
+    .attr("x",6)
+    .attr("y",0)
+    .style("text-anchor","start");
   // Rotación de labels axis principal
 	anchobarra=x2(cd[1].date);
 	context.selectAll(".x.axis text")
@@ -608,6 +580,7 @@ function dibujarZonasHorarias(mostrar) {
   .attr("class","zona"+(zonaverde?"nocturna":"diurna"));
   
 }
+
 function recalcularEstadisticas() {
   // Consultamos primero el estado de la selección
   exttotal=[datos[0].date,datos[datos.length-1].date];
@@ -654,68 +627,67 @@ function calcularkwh(e) {
 
 // Muestra el consumo para la franja seleccionada
 function brushdetend() {
-  ext=brushdet.extent();
-  kwh=calcularkwh(ext);
-  eti=focus.select("#kwhsel");
-  if (kwh>0) {
-//	 alert('Consumo en la franja: '+kwh+" kWh");
-	 
-	 eur=kwh*eurporkwh;
-	 eti=focus.select("#kwhsel");
-	 eti.style("display","inline");
-	 eti.text(kwh.toFixed(2)+" kWh ("+eur.toFixed(2)+" eur)");
-	 xaux=parseInt(focus.select("rect.extent").attr("x"))+1;
-	 yaux=parseInt(focus.select("rect.extent").attr("y")-12);
-	 eti.attr("x",xaux);
-	 eti.attr("y",yaux+10);	 
-  }
-  else
-	 eti.style("display","none");
-	 
+	ext=brushdet.extent();
+	kwh=calcularkwh(ext);
+	eti=focus.select("#kwhsel");
+	if (kwh>0) {
+		//	 alert('Consumo en la franja: '+kwh+" kWh");
+
+		eur=kwh*eurporkwh;
+		eti=focus.select("#kwhsel");
+		eti.style("display","inline");
+		eti.text(kwh.toFixed(2)+" kWh ("+eur.toFixed(2)+" eur)");
+		xaux=parseInt(focus.select("rect.extent").attr("x"))+1;
+		yaux=parseInt(focus.select("rect.extent").attr("y")-12);
+		eti.attr("x",xaux);
+		eti.attr("y",yaux+10);	 
+	}
+	else
+		eti.style("display","none");
 }
 
 // Cuando se acaba la selección. Aquí es donde se hacen las cosas
 function brushend() {
-//  alert('cest fini');
-  ext=brush.extent();
-//  alert('seleccionado de '+ext[0].toLocaleDateString()+' a '+ext[1].toLocaleDateString());
-  ini=lookup[soloFecha(ext[0])];
-  fin=lookup[soloFecha(ext[1])];
-  detalle=datos.slice(ini,fin);
+	//  alert('cest fini');
+	ext=brush.extent();
+	//  alert('seleccionado de '+ext[0].toLocaleDateString()+' a '+ext[1].toLocaleDateString());
+	ini=lookup[soloFecha(ext[0])];
+	fin=lookup[soloFecha(ext[1])];
+	detalle=datos.slice(ini,fin);
 
-  // Recalculamos los ejes para el nuevo intervalo de fechas
-  ext=[detalle[0].date,detalle[detalle.length-1].date];
-  x.domain(ext);
-  intrv=(x.domain()[1] - x.domain()[0]);
-  if (intrv<30*86340000)
-	 xAxis.ticks(d3.time.hours,Math.round(intrv / 86340000) );
-  else
-	 xAxis.ticks(d3.time.days,1);
-  // Por defecto el eje Y no se ajusta, queda más visual
-  //TODO: Añadir un checkbox para seleccionar esto
-//  y.domain([0, 1.2*d3.max(detalle.map(function(d) { return d.w; }))]);
-  focus.select(".x.axis").call(xAxis);  
-  focus.select(".y.axis").call(yAxis);
-  
-  // Eliminar el área y volverla a añadir con los nuevos datos
-  // Seguro que hay una forma mejor de hacerlo, pero así va bien
-  focus.select("#grafica").remove();
-  focus.append("path")
-  .datum(detalle)
-  .attr("clip-path", "url(#clip)")
-  .attr("d", area)
-  .attr("id","grafica");
+	// Recalculamos los ejes para el nuevo intervalo de fechas
+	ext=[detalle[0].date,detalle[detalle.length-1].date];
+	x.domain(ext);
+	intrv=(x.domain()[1] - x.domain()[0]);
+	if (intrv<30*86340000)
+		xAxis.ticks(d3.time.hours,Math.round(intrv / 86340000) );
+	else
+		xAxis.ticks(d3.time.days,1);
+	// Por defecto el eje Y no se ajusta, queda más visual
+	//TODO: Añadir un checkbox para seleccionar esto
+	//  y.domain([0, 1.2*d3.max(detalle.map(function(d) { return d.w; }))]);
+	focus.select(".x.axis").call(xAxis);  
+	focus.select(".y.axis").call(yAxis);
 
-  recalcularEstadisticas();
-  
-  // Redibujamos el máximo
-  dibujarMax(mostrarmax);
-  
-  // Redibujamos las zonas horarias
-  dibujarZonasHorarias(mostrarZonas=="TDH");
-  
-  // Volvemos a aplicar las transformaciones manuales que no se pueden hacer por CSS
-  transformaciones();
+	// Eliminar el área y volverla a añadir con los nuevos datos
+	// Seguro que hay una forma mejor de hacerlo, pero así va bien
+	focus.select("#grafica").remove();
+	focus.append("path")
+		.datum(detalle)
+		.attr("clip-path", "url(#clip)")
+		.attr("d", area)
+		.attr("id","grafica");
+
+	recalcularEstadisticas();
+
+	// Redibujamos el máximo
+	dibujarMax(mostrarmax);
+
+	// Redibujamos las zonas horarias
+	dibujarZonasHorarias(mostrarZonas=="TDH");
+
+	// Volvemos a aplicar las transformaciones manuales que no se pueden hacer por CSS
+	transformaciones();
 }
 
 // Cuando se ajusta la selección. Se redondea siempre al día
@@ -742,17 +714,6 @@ function brushed() {
   }
   
   d3.select(this).call(brush.extent(extent1));  
-//  alert('aa');
-/*  if (brush.empty()) {
-	 x.domain(x2.domain());
-	 xAxis=ajustar(xAxis,x2.domain());
-  } else {
-	 x.domain(brush.extent());
-	 xAxis=ajustar(xAxis,brush.extent());
-  }
-  focus.select("path").attr("d", area);
-  focus.select(".x.axis").call(xAxis);
-  focus.select(".x.axis2").call(xAxisDias);*/
   
   // Redibujamos el máximo
 //  dibujarMax();
@@ -764,9 +725,3 @@ function brushed() {
 //  transformaciones();
 }
 
-// Brush adicional para la gráfica superior. Llama al otro, y sincroniza
-/*function brushed2() {
-  // Sincronizar los extents
-  brush.extent(brush2.extent());
-  d3.select(this).call(brush.event);
-}*/
